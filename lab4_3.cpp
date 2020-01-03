690e1d0b24988e68a628d04a8080cc3d76738a82
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-03 15:24:52 +0700

    numba1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int findDivisor(int x){
+    int i = 2;
+    while (i>1){
+        if (x%i == 0)
+        {
+            return i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+int main (){
+    cout << "Expect = " << findDivisor(221) ;
+    return 0;
+}
