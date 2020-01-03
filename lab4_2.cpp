3d7aac60bf90f076583d201c4af284fd47147a4e
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-03 15:09:05 +0700

    numba 1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
-//Write function findDistance() here
+float findDistance(float u,float a,float t){
+  float s = u*t+0.5*a*pow(t,2);
+  return s;
+}
 
 int main(){
-
-  //Calling findDistance() using test cases
-
+  float s = findDistance(1.5,-1,2);
+  cout << "S = " << s;
   return 0;
 }
