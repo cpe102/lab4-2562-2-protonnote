caf378ea3ecbddc049e402d5a7a296cf0cac555e
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-03 14:56:46 +0700

    numba 1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    float x,y,e=2.71828;
+    cout << "Enter x = ";
+    cin >> x;
+    y = 3*pow(x,3)+2*e+pow(2,(2*x+1))+pow(pow(x,2)+1,0.5);
+    cout << "Result y = " << y ;
+    return 0;
+}
